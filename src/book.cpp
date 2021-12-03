48da4c61debc0e67886ae5c17e5af680931aee30
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2021-12-03 16:06:15 +0100

    LibrarySystemCpp (v0.3)     *Add       -Constructor in book.cpp       -Function printInfo     *Updates       -main.cpp       -Comments

--- src/book.cpp
+++ src/book.cpp
@@ -1,10 +1,11 @@
 //
 // Created by Jakub Sitarczyk on 02/12/2021.
 //
+#include <iostream>
 #include <string>
 
 using namespace std;
-class Book{ //Book class
+class Book{ //Book class®
   public:
     string title;
     string author;
@@ -12,4 +13,17 @@ class Book{ //Book class
     int pages;
     string publisher;
     string isbn;
+    Book(string title, string author, int releaseDate, int pages, string publisher, string isbn){ //Constructor Book
+      this->title = title;
+      this->author = author;
+      this->releaseDate = releaseDate;
+      this->pages = pages;
+      this->publisher = publisher;
+      this->isbn = isbn;
+    }
+  void printInfo(){ //Printer
+    string info = title + "; " + author + "; " + to_string(releaseDate) + "; " + to_string(pages) + "; " + publisher + "; " + isbn;
+    cout << info << endl;
+  }
 };
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,21 @@
 //
 // Created by Jakub Sitarczyk on 02/12/2021.
 //
-//Libs
-#include <iostream>
 //Files
 #include "book.cpp"
+//Libs
+#include <iostream>
 
 using namespace std;
 int main() {
-  const string appName = "Biblioteka v0.2";
+  const string appName = "Biblioteka v0.3";
 
-  Book book;  //New Class
-  book.title = "W pustyni i w puszczy";
-  book.author = "Henryk Sienkiewicz";
-  book.releaseDate = 2010;
-  book.pages = 296;
-  book.publisher = "Greg";
-  book.isbn = "9788373271890";
+  Book book1("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg","9788373271890");  //Create Book
+  Book book2("Opus magnum C++ 11", "Jerzy Grębosz", 2020, 1648, "Helion", "9788328371026");
 
   cout << appName << endl;
-  cout << "Książki dostępne w bibliotece:" << endl;
-  cout << book.title << endl;
-  cout << book.author << endl;
-  cout << book.releaseDate << endl;
-  cout << book.pages << endl;
-  cout << book.publisher << endl;
-  cout << book.isbn << endl;
+  book1.printInfo();  //Print information
+  book2.printInfo();
 
   return 0;
 }
