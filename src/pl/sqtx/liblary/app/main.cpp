6ab80daf4c02641f0d081c8be01c3afe99ec4274
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2021-12-04 12:40:22 +0100

    LibrarySystemCpp (v0.5)         *Add           -Getters and Setters

--- src/pl/sqtx/liblary/app/main.cpp
+++ src/pl/sqtx/liblary/app/main.cpp
@@ -8,12 +8,14 @@
 
 using namespace std;
 int main() {
-  const string appName = "Biblioteka v0.45";
+  const string appName = "Biblioteka v0.5";
 
-  Book book1("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg","9788373271890");  //Create Book
-  Book book2("Opus magnum C++ 11", "Jerzy Grębosz", 2020, 1648, "Helion", "9788328371026");
+  Book book1("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
+             296, "Greg","9788373271890");  //Create Book
+  Book book2("Opus magnum C++ 11", "Jerzy Grębosz", 2020,
+             1648, "Helion", "9788328371026");
   Book book3("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                        2008, 851, "McGraw-Hill Osborne Media");
+             2008, 851, "McGraw-Hill Osborne Media");
   cout << appName << endl;
   book1.printInfo();  //Print information
   book2.printInfo();
--- src/pl/sqtx/liblary/model/book.cpp
+++ src/pl/sqtx/liblary/model/book.cpp
@@ -5,22 +5,24 @@
 #include <string>
 
 using namespace std;
-class Book{ //Book class®
-  public:
+class Book{ //Book class
+private:
     string title;
     string author;
     int releaseDate;
     int pages;
     string publisher;
     string isbn;
-    Book(string title, string author, int releaseDate, int pages, string publisher) { //Constructor Book
+public:
+//    Constructors  *****************************************************************************************
+    Book(string title, string author, int releaseDate, int pages, string publisher) {
       this->title = title;
       this->author = author;
       this->releaseDate = releaseDate;
       this->pages = pages;
       this->publisher = publisher;
     }
-    Book(string title, string author, int releaseDate, int pages, string publisher, string isbn) { //Constructor Book
+    Book(string title, string author, int releaseDate, int pages, string publisher, string isbn) {
       this->title = title;
       this->author = author;
       this->releaseDate = releaseDate;
@@ -28,9 +30,48 @@ class Book{ //Book class®
       this->publisher = publisher;
       this->isbn = isbn;
     }
-  void printInfo(){ //Printer
-    string info = title + "; " + author + "; " + to_string(releaseDate) + "; " + to_string(pages) + "; " + publisher + "; " + isbn;
-    cout << info << endl;
-  }
+//  Getter and Setter *****************************************************************************************
+    const string &getTitle() const {
+      return title;
+    }
+    void setTitle(const string &title) {
+      Book::title = title;
+    }
+    const string &getAuthor() const {
+      return author;
+    }
+    void setAuthor(const string &author) {
+      Book::author = author;
+    }
+    int getReleaseDate() const {
+      return releaseDate;
+    }
+    void setReleaseDate(int releaseDate) {
+      Book::releaseDate = releaseDate;
+    }
+    int getPages() const {
+      return pages;
+    }
+    void setPages(int pages) {
+      Book::pages = pages;
+    }
+    const string &getPublisher() const {
+      return publisher;
+    }
+    void setPublisher(const string &publisher) {
+      Book::publisher = publisher;
+    }
+    const string &getIsbn() const {
+      return isbn;
+    }
+    void setIsbn(const string &isbn) {
+      Book::isbn = isbn;
+    }
+//    Function *****************************************************************************************
+    void printInfo(){ //Printer
+      string info = title + "; " + author + "; " + to_string(releaseDate) + "; " + to_string(pages) +
+          "; " + publisher + "; " + isbn;
+      cout << info << endl;
+    }
 };
 
