413c050ee24b850a355fc8007fbe97f388c2d177
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2022-02-15 18:45:12 +0100

    save

--- src/pl/sqtx/liblary/io/file/serializableFileManager.cpp
+++ src/pl/sqtx/liblary/io/file/serializableFileManager.cpp
@@ -2,10 +2,18 @@
 // Created by Jakub Sitarczyk on 15/02/2022.
 //
 #include "../../include/main_h.h"
+#include "../../include/exception_h.h"
 #include "fileManager.cpp"
+#include <fstream>
 
 class SerializableFileManager : public FileManager{
+  fstream dataFile;
   void importData(){
+    dataFile.open("save.txt", ios::in);
+    if(dataFile.is_open()){
 
+    }else{
+
+    }
   }
 };
