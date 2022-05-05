83ce789601fdfbc74f2b711d3373fdc8d8371f24
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2022-05-05 20:20:31 +0200

    Potential search engine bug fix

--- src/pl/sqtx/library/engine/searchEngine.cpp
+++ src/pl/sqtx/library/engine/searchEngine.cpp
@@ -11,7 +11,7 @@ int searchEngine::findUser(libraryUser *libraryUser, string txt, int lastIterato
   vector<User>::iterator it_users = libraryUser->getIteratorUsers();
 
   ++lastIteratorIndex;
-  if(!(lastIteratorIndex > libraryUser->getUsersVector().size())){
+  if (lastIteratorIndex <= libraryUser->getUsersVector().size()) {
     for (int i = lastIteratorIndex; i < libraryUser->getUsersVector().size(); i++) {
 //      Transform user data to the same case
       const string firstname = dataReader.toLowerCase((it_users + i)->getFirstName());
@@ -21,15 +21,18 @@ int searchEngine::findUser(libraryUser *libraryUser, string txt, int lastIterato
 
       if (fromUser == firstname || fromUser == lastname || fromUser == pesel || fromUser == firstNLastName) return i;
     }
+  } else {
+    return -1;
   }
   return -1;
 }
+
 int searchEngine::findPublication(library *library, std::string txt, int lastIteratorIndex) {
   string fromUser = dataReader.toLowerCase(txt);
   vector<PublicationPtr>::iterator it_publication = library->getIteratorPublications();
 
   ++lastIteratorIndex;
-  if(!(lastIteratorIndex > library->getPublications().size())){
+  if (lastIteratorIndex <= library->getPublications().size()) {
     for (int i = lastIteratorIndex; i < library->getPublications().size(); i++) {
 //      Transform user data to the same case
       const string title = dataReader.toLowerCase((it_publication + i)->get()->getTitle());
@@ -45,6 +48,8 @@ int searchEngine::findPublication(library *library, std::string txt, int lastIte
 
       if (fromUser == title || fromUser == secondPart || fromUser == publisher || fromUser == isbn) return i;
     }
+  } else {
+    return -1;
   }
   return -1;
 }
@@ -60,15 +65,14 @@ void searchEngine::searchEng(library *library, libraryUser *libraryUser) {
   cslPrinter.printLine("Uzytkownicy:");
   do {
     wantedIteratorIndex = findUser(libraryUser, txt, wantedIteratorIndex);
-    if(wantedIteratorIndex >= 0) cslPrinter.printLine((it_user + wantedIteratorIndex)->toString());
+    if (wantedIteratorIndex >= 0) cslPrinter.printLine((it_user + wantedIteratorIndex)->toString());
   } while (wantedIteratorIndex >= 0);
 
+  wantedIteratorIndex = -1;
   cslPrinter.printLine("Publikacje:");
-  if (wantedIteratorIndex < 0){
-   vector<PublicationPtr>::iterator it_publication = library->getIteratorPublications();
-    do {
-      wantedIteratorIndex = findPublication(library, txt, wantedIteratorIndex);
-      if(wantedIteratorIndex >= 0) cslPrinter.printLine((it_publication + wantedIteratorIndex)->get()->toString());
-    } while (wantedIteratorIndex >= 0);
-  }
+  vector<PublicationPtr>::iterator it_publication = library->getIteratorPublications();
+  do {
+    wantedIteratorIndex = findPublication(library, txt, wantedIteratorIndex);
+    if (wantedIteratorIndex >= 0) cslPrinter.printLine((it_publication + wantedIteratorIndex)->get()->toString());
+  } while (wantedIteratorIndex >= 0);
 }
